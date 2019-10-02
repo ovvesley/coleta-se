974f36aef4f9b73089b62f391a60ecda2543fd56
Leorna <gabrielcapuerj@gmail.com>
2019-10-02 18:03:40 -0300

    adicionando lista

--- frontend/coleta-se/src/screens/List/index.js
+++ frontend/coleta-se/src/screens/List/index.js
@@ -1,25 +1,68 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 
 export default class List extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      spots: [],
+      loading: true
+    };
+  }
+
+  async getSpots() {
+    try {
+      const spotsApi = await fetch('https://localhost:1234/src/main.php');
+      const spot = await spotsApi.json();
+      this.setState({ spots: spot.results, loading: false});
+      alert(spot.results);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  renderItem(data) {
+    return (
+      <Text>{data.name, data.age}</Text>
+    );
   }
 
   render() {
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center"
-      }
-    });
-    return (
-      <View style={styles.container}>
-        <Text>ListPage</Text>
-      </View>
-    );
+    
+    const { spots, loading } = this.state;
+    
+    if (!loading) {
+      return (
+        <View style={styles.container}>
+          <FlatList
+            data={spots}
+            renderItem={this.renderItem}
+            onEndReached={this.getSpots}
+          />
+        </View>
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text>No data</Text>
+        </View>
+      );
+    }
   }
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#ffffff"
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+});
