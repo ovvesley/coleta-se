fb8adaa5f0d5c49c129f5d57f7f6e984ca2118bb
Leorna <gabrielcapuerj@gmail.com>
2019-10-02 21:29:49 -0300

    transformando o List em stateless

--- frontend/coleta-se/src/screens/List/index.js
+++ frontend/coleta-se/src/screens/List/index.js
@@ -1,89 +1,20 @@
-import React, { Component } from "react";
-import { View, Text, StyleSheet, FlatList } from "react-native";
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+import { styles } from "./styles"; 
 
-export default class List extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      spots: [],
-      loading: true
-    };
-  }
-
-  async getSpots() {
-    try {
-      const spotsApi = await fetch('https://api.github.com/users', {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credetials: "same-origin"
-      });
-      return await spotsApi.json();
-    } catch (error) {
-      console.log(error);
-    }
-  }
-
-  componentDidMount() {
-    this.getSpots()
-    .then(data => {
-      //console.log(data);
-      for (let i = 0; i < data.length; i++) {
-        data[i]['key'] = i+'a';
-      }
-    
-      this.setState({ spots: data, loading: false });  
-    });
-  }
-
-  renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text>{item.login}</Text>
+export default function List({ spots }) {
+  return spots && spots.length > 0 ? (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.lista}
+        contentContainerStyle={styles.container}
+        data={spots}
+        renderItem={({ item }) => <Text style={styles.item}>{item.login}</Text>}
+      />
     </View>
+  ) : (
+      <View style={styles.container}>
+        <Text>No data</Text>
+      </View>
   );
-
-  render() {
-
-    const { spots, loading } = this.state;
-    console.log(spots);
-
-
-    return !loading ? (
-        <View style={styles.container}>
-          <FlatList
-            style={styles.lista}
-            contentContainerStyle={styles.container}
-            data={spots}
-            renderItem={ ({item}) => <Text style={styles.item}>{ item.login }</Text> }
-          />
-        </View>
-      ) : (
-        <View style={styles.container}>
-          <Text>No data</Text>
-        </View>
-    );
-  }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 20,
-    backgroundColor: '#8000FF',
-  },
-
-  item: {
-    backgroundColor: '#EEE',
-    marginTop: 20,
-    padding: 30,
-    textAlign: "center",
-    fontSize: 20,
-  },
-
-  lista: {
-    marginTop: 0,
-    padding: 0,
-  },
-
-});
---
+++ frontend/coleta-se/src/screens/List/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+      paddingHorizontal: 20,
+      backgroundColor: '#8000FF',
+    },
+  
+    item: {
+      backgroundColor: '#EEE',
+      marginTop: 20,
+      padding: 30,
+      textAlign: "center",
+      fontSize: 20,
+    },
+  
+    lista: {
+      marginTop: 0,
+      padding: 0,
+    },
+  
+});
