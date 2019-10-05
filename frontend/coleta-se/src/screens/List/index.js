abe73b15cae351299efb3eaac1b5b749593a04ac
Leorna <gabrielcapuerj@gmail.com>
2019-10-05 12:26:15 -0300

    botando click

--- frontend/coleta-se/src/screens/List/index.js
+++ frontend/coleta-se/src/screens/List/index.js
@@ -1,15 +1,26 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
 import { styles } from "./styles";
 
 export default function List({ spots }) {
+
+  function onPress(item) {
+    console.log(item);
+  }
+
+
+
   return spots && spots.length > 0 ? (
     <View style={styles.container}>
       <FlatList
         style={styles.lista}
         contentContainerStyle={styles.container}
         data={spots}
-        renderItem={({ item }) => <Text style={styles.item}>{item.login}</Text>}
+        renderItem={({ item }) => (
+          <TouchableOpacity style={styles.item} onPress={() => onPress(item)}>
+            <Text>{item.login}</Text>
+          </TouchableOpacity>
+        )}
       />
     </View>
   ) : (
