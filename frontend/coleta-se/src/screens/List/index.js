86b3658af264c30e0deb602a2cb46f07b0a7373a
Leorna <gabrielcapuerj@gmail.com>
2019-10-05 13:31:22 -0300

    adicionando card

--- frontend/coleta-se/src/screens/List/index.js
+++ frontend/coleta-se/src/screens/List/index.js
@@ -1,25 +1,22 @@
 import React from "react";
-import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import { Card } from "react-native-elements";
 import { styles } from "./styles";
 
 export default function List({ spots }) {
 
-  function onPress(item) {
-    console.log(item);
-  }
-
-
-
   return spots && spots.length > 0 ? (
-    <View style={styles.container}>
-      <FlatList
-        style={styles.lista}
-        contentContainerStyle={styles.container}
+    <View>
+      <FlatList 
         data={spots}
         renderItem={({ item }) => (
-          <TouchableOpacity style={styles.item} onPress={() => onPress(item)}>
-            <Text>{item.login}</Text>
-          </TouchableOpacity>
+          <Card 
+            style={styles.card} 
+            title={item.titulo}>
+            <Text>
+              {item.descricao}
+            </Text>
+          </Card>
         )}
       />
     </View>
