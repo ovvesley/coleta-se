4edcebeb0549d650d988ed9959419ff40219ab1c
vvesly <wsf.ley@gmail.com>
2019-10-11 22:21:55 -0300

    Att Index.

--- frontend/coleta-se/src/screens/Mapa/index.js
+++ frontend/coleta-se/src/screens/Mapa/index.js
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet, Image } from "react-native";
+import { FAB, Portal, Provider } from "react-native-paper";
 import MapView, { Marker } from "react-native-maps";
 import styles from "./styles";
+import FloatButtonGroup from "../../Components/GenericComponent/FloatButtonGroup";
+import ModalForm from "../../Components/GenericComponent/ModalForm";
+
 export default class Mapa extends Component {
   constructor(props) {
     super(props);
@@ -12,7 +16,10 @@ export default class Mapa extends Component {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
       },
-      markers: []
+      markers: [],
+      modal: {
+        visible: false
+      }
     };
   }
   componentDidMount() {
@@ -36,29 +43,42 @@ export default class Mapa extends Component {
   onRegionChange(region) {
     this.setState({ region });
   }
+  _showModal = () => this.setState({ modal: { visible: true } });
+  _hideModal = () => this.setState({ modal: { visible: false } });
 
   render() {
-    
+    const FloatButton = () => (
+      <FAB
+        style={styles.fab}
+        icon="plus-one"
+        onPress={() => console.log("Pressed")}
+      />
+    );
     return (
-      <MapView
-        style={styles.mapContainer}
-        region={this.state.region}
-        showsUserLocation
-      >
-        {this.state.markers.map(marker => (
-          <Marker
-            coordinate={marker.coordinate}
-            title={marker.title}
-            description={marker.description}
-            key={marker.id}
-          >
-            <Image
-              source={require("../../../assets/carro_de_lixo.png")}
-              style={{ width: 40, height: 40 }}
-            />
-          </Marker>
-        ))}
-      </MapView>
+      <View style={{flex: 1}}>
+        <MapView
+          style={{width:'100%', height:'100%'}}
+          region={this.state.region}
+          showsUserLocation
+        >
+          {this.state.markers.map(marker => (
+            <Marker
+              coordinate={marker.coordinate}
+              title={marker.title}
+              description={marker.description}
+              key={marker.id}
+            >
+              <Image
+                source={require("../../../assets/carro_de_lixo.png")}
+                style={{ width: 40, height: 40 }}
+              />
+            </Marker>
+          ))}
+        </MapView >
+
+        <FloatButtonGroup showModal={this._showModal} />
+        <ModalForm visible={this.state.modal.visible} hideModal={this._hideModal}/>
+      </View>
     );
   }
 }
