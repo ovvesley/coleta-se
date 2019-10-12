eae1dab56c3e70b2deecbe24eabdb59f2b65e627
vvesly <wsf.ley@gmail.com>
2019-10-11 22:22:03 -0300

    Styles att.

--- frontend/coleta-se/src/screens/Mapa/styles.js
+++ frontend/coleta-se/src/screens/Mapa/styles.js
@@ -13,5 +13,11 @@ export default styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
-  }
+  },
+  fab: {
+    position: 'absolute',
+    margin: 16,
+    right: 0,
+    bottom: 0,
+  },
 });
